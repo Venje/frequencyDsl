668d8c916c14c78278b9154d796a2b419fabf74b
AlexeyVorobyev <flyenj@gmail.com>
2015-02-18 21:30:33 +0600

    добавляет вариант с точкой

--- src/main/scala/org/nd/frequency/MainApp.scala
+++ src/main/scala/org/nd/frequency/MainApp.scala
@@ -8,14 +8,19 @@ package org.nd.frequency
 object MainApp {
 
   def main(args: Array[String]): Unit = {
+
+    // Постфиксная нотация. Не рекомендуется.
     println(1 Hz)
     println(1 kHz)
     println(1 MHz)
     println(1 GHz)
 
-    // Скобки необходимы
     val sum = (3000 kHz) + (2 MHz)
 
+    // Безопасная нотация.
+    val sum2 = 2000.kHz + 200.kHz
+
     println("3000 kHz + 2 MHz equals " + sum.toKHz)
+    println("2000 kHz + 200 kHz equals " + sum2.toKHz)
   }
 }
